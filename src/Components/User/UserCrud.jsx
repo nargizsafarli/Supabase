fee9b3de00225217c9cc78452960518babec3e0a
Nergiz Seferli <nergizsafarli60@gmail.com>
2025-04-22 23:00:24 +0400

    crud operation added

--- src/Components/User/UserCrud.jsx
+++ src/Components/User/UserCrud.jsx
@@ -1,9 +1,112 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { GlobalContext } from "../../Context/GlobalContext";
 
 function UserCrud() {
+  const { users, createUser, deleteUser, updateUser } =
+    useContext(GlobalContext);
+  const [editMode, setEditMode] = useState(false);
+  const [editId, setEditId] = useState(null);
+
+  const [user, setUser] = useState({
+    name: "",
+    surname: "",
+    age: "",
+  });
+
+  const handleChange = (e) => {
+    setUser((preventDefault) => {
+      return {
+        ...preventDefault,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (editMode) {
+      await updateUser({ ...user, id: editId });
+      setEditMode(false);
+      setEditId(null);
+    } else {
+      await createUser(user);
+    }
+
+    setUser({ name: "", surname: "", age: "" });
+  };
+
+  const handleDelete = async (id) => {
+    await deleteUser(id);
+  };
+
+  const handleEdit = (user) => {
+    setUser({ name: user.name, surname: user.surname, age: user.age });
+    setEditMode(true);
+    setEditId(user.id);
+  };
+
   return (
-    <div>UserCrud</div>
-  )
+    <div>
+      <h2>User List</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={user.name}
+          name="name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Surname"
+          value={user.surname}
+          name="surname"
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="Age"
+          value={user.age}
+          name="age"
+          onChange={handleChange}
+        />
+        <button type="submit">{editMode ? "save" : "Create"}</button>
+      </form>
+
+      <table border="1" cellPadding="10" cellSpacing="0">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users?.map((userItem) => (
+            <tr key={userItem.id}>
+              <td>{userItem.name}</td>
+              <td>{userItem.surname}</td>
+              <td>{userItem.age}</td>
+              <td>
+                {editId === userItem.id ? (
+                  <span>Editing...</span>
+                ) : (
+                  <>
+                    <button onClick={() => handleDelete(userItem.id)}>
+                      Delete
+                    </button>
+                    <button onClick={() => handleEdit(userItem)}>Edit</button>
+                  </>
+                )}
+              </td>
+                
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default UserCrud
+export default UserCrud;
--- src/Context/GlobalContext.jsx
+++ src/Context/GlobalContext.jsx
@@ -1,37 +1,100 @@
 import { createContext, useEffect, useState } from "react";
 import { supabase } from "../client";
 
-export const GlobalContext=createContext();
-export const GlobalProvider=({children})=>{
+export const GlobalContext = createContext();
+export const GlobalProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  const [users, setUsers] = useState([]);
+  console.log(users, "users");
+  const [page, setPage] = useState(1);
+  const limit = 6;
 
-    const [products, setProducts] = useState([]);
-    const [page, setPage] = useState(1);
-    const limit = 6;
-    console.log(products);
-    const fetchProducts = async (page) => {
-        const from = (page - 1) * limit;
-        const to = from + limit - 1;
-    
-        const { data, error } = await supabase
-          .from("Products")
-          .select("*")
-          .range(from, to);
-    
-        if (error) {
-          console.error("Supabase error:", error.message);
-        } else {
-          setProducts(data);
-        }
-      };
-      useEffect(() => {
-        fetchProducts(page);
-      }, [page]);
+  const fetchProducts = async (page) => {
+    const from = (page - 1) * limit;
+    const to = from + limit - 1;
+
+    const { data, error } = await supabase
+      .from("Products")
+      .select("*")
+      .range(from, to);
+
+    if (error) {
+      console.error("Supabase error:", error.message);
+    } else {
+      setProducts(data);
+    }
+  };
+  useEffect(() => {
+    fetchProducts(page);
+  }, [page]);
+
+
+//   ! ------------------------User--------------------------------------
+
+  const fetchUsers = async () => {
+    const { data, error } = await supabase.from("users").select("*");
+    if (error) {
+      console.error("Supabase error:", error.message);
+    } else {
+      setUsers(data);
+    }
+  };
+  const createUser = async (newUser) => {
+    const { data, error } = await supabase.from("users").insert([newUser]);
+
+    if (error) {
+      console.error("Create user error:", error.message);
+    } else {
+      fetchUsers();
+    }
+  };
+
+  const deleteUser = async (id) => {
+    const { error } = await supabase.from("users").delete().eq("id", id);
+
+    if (error) {
+      console.log("delete Error", error.message);
+    } else {
+      fetchUsers();
+    }
+  };
+
+  const updateUser = async (updateUser) => {
+    const { id, name, surname, age } = updateUser;
+    const { error } = await supabase
+      .from("users")
+      .update({ name, surname, age })
+      .eq("id", id);
+    if (error) {
+      console.log("Update Error", error.message);
+    } else {
+      fetchUsers();
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+//   ! ---------------------------------Auth-----------------------------------
 
 
 
-    return (
-        <GlobalContext.Provider value={{products,setPage}}>
-            {children}
-        </GlobalContext.Provider>
-    )
-}
+
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        products,
+        setPage,
+        users,
+        fetchUsers,
+        createUser,
+        deleteUser,
+        updateUser,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
